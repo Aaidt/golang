8d6a74d29ce3f927b1635f22ab3053f60bc79356
Aadit <aadits2004@gmail.com>
2025-08-10 22:11:18 +0530

    hacky way to stop the main fn from ending before the expensive operation ends

--- main.go
+++ main.go
@@ -1,28 +1,18 @@
 package main
 
-func main() {
-	// ch := make(chan int)
-	//
-	// go func() {
-	// 	ch <- 5
-	// }()
-	//
-	// value := <-ch
-	// println(value)
-
-	ch := make(chan int, 5)
-
-	go func() {
-		defer close(ch)
-		for i := range 10 {
-			ch <- i
-		}
-	}()
-
-	val, ok := <-ch
-	println(val, ok)
-
-	for v := range ch {
-		println(v)
+func expensiveoperation(ch chan<- bool) {
+	sum := 0
+	for i := range 100000000000 {
+		sum += i
 	}
+	println(sum)
+	ch <- true
+}
+
+func main() {
+	ch := make(chan bool)
+
+	go expensiveoperation(ch)
+	<-ch
+	println("Main function ended.")
 }

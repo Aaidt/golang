7f9cf5f90e9eb0628c92396af1b8903e9a62d412
Aadit <aadits2004@gmail.com>
2025-08-11 19:07:01 +0530

    basic http server

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
 )
 
-type Person struct {
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
-	Email string `json:"email,omitempty"`
+func Hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "hello")
 }
 
 func main() {
-	person := Person{Name: "Aadit", Age: 34, Email: ""}
+	http.HandleFunc("/hello", Hello)
 
-	jsonData, err := json.Marshal(person)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(jsonData))
+	http.ListenAndServe(":3000", nil)
 }

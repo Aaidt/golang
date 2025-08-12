4c6f59803d7fe28264e2c969e7d3339ef13b28de
Aadit <aadits2004@gmail.com>
2025-08-12 15:13:28 +0530

    select for managing output of channels

--- main.go
+++ main.go
@@ -2,52 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-type shape interface {
-	area() float32
-	perimeter() float32
-}
-
-type rect struct {
-	length  float32
-	breadth float32
-}
-
-type square struct {
-	side float32
-}
-
-func (r rect) area() float32 {
-	return r.length * r.breadth
-}
-
-func (r rect) perimeter() float32 {
-	return 2 * (r.length + r.breadth)
-}
-
-func (s square) area() float32 {
-	return s.side * s.side
-}
-
-func (s square) perimeter() float32 {
-	return 4 * s.side
-}
-
-func calculateArea(s shape) {
-	fmt.Println("The area is: ", s.area())
-}
-
-func calculatePerimeter(s shape) {
-	fmt.Println("THe perimeter is: ", s.perimeter())
-}
-
 func main() {
-	r := rect{length: 10, breadth: 20}
-	sq := square{side: 30}
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
-	calculateArea(sq)
-	calculateArea(r)
-	calculatePerimeter(sq)
-	calculatePerimeter(r)
+	go func() {
+		time.Sleep(time.Second * 2)
+		ch1 <- 1
+	}()
+
+	go func() {
+		time.Sleep(time.Second)
+		ch2 <- 2
+	}()
+
+	for range 2 {
+		select {
+		case first := <-ch1:
+			fmt.Println(first)
+		case second := <-ch2:
+			fmt.Println(second)
+		}
+	}
 }

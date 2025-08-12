22b9255ac1016f69ee1ae6fe809f3145693272e1
Aadit <aadits2004@gmail.com>
2025-08-12 15:39:29 +0530

    fetching data from jsonplaceholder

--- main.go
+++ main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
+	"io"
+	"net/http"
 )
 
-func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+type post struct {
+	UserId int    `json:"userId"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
 
-	go func() {
-		time.Sleep(time.Second * 2)
-		ch1 <- 1
-	}()
-
-	go func() {
-		time.Sleep(time.Second)
-		ch2 <- 2
-	}()
-
-	for range 2 {
-		select {
-		case first := <-ch1:
-			fmt.Println(first)
-		case second := <-ch2:
-			fmt.Println(second)
-		}
+func SendRequest(url string, ch chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- "Error while sending request"
 	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		ch <- "Error while reading body"
+	}
+
+	p := post{}
+
+	json.Unmarshal(body, &p)
+
+	ch <- p.Title
+}
+
+func main() {
+	ch := make(chan string)
+	go SendRequest("https://jsonplaceholder.typicode.com/posts/1", ch)
+
+	ans := <-ch
+
+	fmt.Println(ans)
 }
